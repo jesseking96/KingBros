44af82d69b6e9efa8065104602275c2aa0842329
Sky <metalandhotdogs@yahoo.com>
2020-09-28 09:40:20 -0500

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kingbros.wsgi
--- kingbros/settings.py
+++ kingbros/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import django_heroku
 env = Env()
 env.read_env()
 
@@ -28,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG',default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
@@ -148,3 +149,5 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS
 SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD",default=True)
 SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE",default=True)
 CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE",default=True)
+
+django_heroku.settings(locals())
