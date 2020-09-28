58c2b2c7bdcf81f446d4551f4cfad99c5606def9
Sky <metalandhotdogs@yahoo.com>
2020-09-28 13:42:54 -0500

    update environment variables

--- kingbros/settings.py
+++ kingbros/settings.py
@@ -143,7 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "jesseking0111@gmail.com"
-EMAIL_HOST_PASSWORD = "gosvckcjnrmgembf"
+EMAIL_HOST_PASSWORD = env.read('EMAIL_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
