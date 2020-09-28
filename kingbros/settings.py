62f7788f5aa9bd8d8446dc81fe7af64d4c1f4324
Sky <metalandhotdogs@yahoo.com>
2020-09-28 12:03:36 -0500

    edits for production

--- kingbros/settings.py
+++ kingbros/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    #3rd party
+    'whitenoise',
     #local
     'stalls',
     'pages',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kingbros.urls'
