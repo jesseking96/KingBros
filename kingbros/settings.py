00860f765cd3d1f627f0db01a5aef74bc680be37
Sky <metalandhotdogs@yahoo.com>
2020-09-28 11:59:04 -0500

    change db to postgresql

--- kingbros/settings.py
+++ kingbros/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'kingbros.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kingbros',
+        'USER': 'kingbrosadmin',
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT':'',
     }
 }
 
